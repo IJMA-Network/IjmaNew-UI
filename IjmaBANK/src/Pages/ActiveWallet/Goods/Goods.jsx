be7113abf83ae207c166a83c6a26d40eb5450424
Raza <71090111+Raza2468@users.noreply.github.com>
2022-08-28 18:34:47 +0500

    BankUi

---
+++ IjmaBANK/src/Pages/ActiveWallet/Goods/Goods.jsx
@@ -0,0 +1,141 @@
+import React from 'react'
+import './Goods.css'
+import GoodState from './GoodsState.json'
+
+
+export default function Goods() {
+
+  GoodState.map((v, i) => { console.log(v, "GoodState") })
+
+  return (
+    <div>
+      <div class="card card-cascade narrower">
+        <div
+          class="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 mb-3 d-flex justify-content-between align-items-center">
+
+          <div>
+            <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
+              <i class="fas fa-th-large mt-0"></i>
+            </button>
+            <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
+              <i class="fas fa-columns mt-0"></i>
+            </button>
+          </div>
+
+          <a class="white-text mx-3">Allow Access</a>
+
+          <div>
+            <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
+              <i class="fas fa-pencil-alt mt-0"></i>
+            </button>
+            <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
+              <i class="far fa-trash-alt mt-0"></i>
+            </button>
+            <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
+              <i class="fas fa-info-circle mt-0"></i>
+            </button>
+          </div>
+
+        </div>
+        <div class="container mt-3">
+          <h2 className='text-center'>Goods</h2>
+
+          <table class="table table-hover">
+            <thead class="bg-light">
+              <tr>
+                <th>Consignmento.</th>
+                <th>Asset</th>
+                <th>Quantity</th>
+                <th>Insurance</th>
+                <th></th>
+              </tr>
+            </thead>
+            {GoodState.map((v, i) => {
+              return (
+                <tbody>
+                  <tr>
+                    <td>{v.internalReference}</td>
+                    <td>{v.asset}</td>
+                    <td>{v.quantity.value}</td>
+                    <td></td>
+
+                    <td>
+                      <span type="button" class="btn btn-warning btn-rounded" data-toggle="modal" data-target="#myModal"
+                      //  onClick={() => setClinetID(v._id)}
+                      >View</span>
+                    </td>
+
+                  </tr>
+                </tbody>
+              )
+            })}
+
+
+          </table>
+        </div>
+      </div>
+
+
+      <div class="modal" id="myModal">
+        <div class="modal-dialog modal-dialog-scrollable-sm">
+          <div class="modal-content" style={{ width: "115%" }}>
+
+            {/* <!-- Modal Header --> */}
+            <div class="modal-header">
+              <h3 class="modal-title">Murabaha Agreements Detail</h3>
+              <button type="button" class="btn btn-danger close" data-dismiss="modal">X</button>
+            </div>
+
+            {/* <!-- Modal body --> */}
+            {GoodState.map((v, i) => {
+              return (
+                <div class="modal-body">
+                  <table id="customers">
+
+                    <tr>
+                      <th>Company</th>
+                      <th>Contact</th>
+                    </tr>
+                    <tr>
+                      <td>Seller</td>
+                      <td>{v.vendor.name}</td>
+                    </tr>
+                    <tr>
+                      <td>Asset</td>
+                      <td>{v.description}</td>
+                    </tr>
+                    <tr>
+                      <td>Quantity</td>
+                      <td>{v.quantity.value}</td>
+                    </tr>
+                    <tr>
+                      <td>Reedemable</td>
+                      <td>Yes</td>
+                    </tr>
+                    <tr>
+                      <td>Issure</td>
+                      <td>No</td>
+                    </tr>
+
+
+                  </table>
+
+                </div>
+              )
+            })}
+
+
+
+            {/* <!-- Modal footer --> */}
+            <div class="modal-footer">
+              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
