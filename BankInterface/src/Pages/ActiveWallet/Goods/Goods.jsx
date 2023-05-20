0d17d82537ce60de125f58b23306b6166d6e7ae2
MuhammadHammadAhmed <hammad.tco@gmail.com>
2023-05-20 17:47:17 +0500

    updated goods page in seller and bank

--- BankInterface/src/Pages/ActiveWallet/Goods/Goods.jsx
+++ BankInterface/src/Pages/ActiveWallet/Goods/Goods.jsx
@@ -1,33 +1,65 @@
-import React from 'react'
-import Filter from '../../filter/filter'
-import './Goods.css'
-import GoodState from './GoodsState.json'
-
+import React, { useState, useContext } from 'react'
+import Filter from '../../filter/filter';
+import './Goods.css';
+import GoodState from './GoodsState.json';
+import { ToastContainer, toast } from 'react-toastify';
+import StoreContext from '../../../ContextApi';
+import { Button, message, Space, Spin } from 'antd';
+import Modal from 'react-bootstrap/Modal';
 
 export default function Goods() {
-
-
-  const [user, setUser] = useState({ accountName: "Bank1" });
   const [goods, setGoods] = useState(GoodState);
-  const [item, setItem] = useState(null);
   const [loading, setloading] = useState(true);
   const contextData = useContext(StoreContext);
-  GoodState.map((v, i) => { console.log(v, "GoodState") })
+  const [item, setItem] = useState(null);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  console.log(contextData.SignInData, "Good Context Data");
+
+  const notify = () => toast.success('🦄 Successfully!', {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+
+  goods.map((v, i) => { console.log(v, "GoodState") })
+
+  const Redeem = () => {
+    setloading(false)
+
+    setTimeout(() => {
+      setloading(true) // 1
+      handleClose() // 2
+      notify() // 3
+    }, 2000);
+  }
+  // const notify = () => toast.success('🦄 Wow so easy!', {
+  //   position: "top-right",
+  //   autoClose: 5000,
+  //   hideProgressBar: false,
+  //   closeOnClick: true,
+  //   pauseOnHover: true,
+  //   draggable: true,
+  //   progress: undefined,
+  //   theme: "light",
+  // })
 
   return (
     <div>
       <div class="card card-cascade narrower">
         <Filter />
-
-        <div class="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 mb-3 d-flex justify-content-between align-items-center"
-         style={{ marginTop: "-5%" }}
+        <div
+          class="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 d-flex justify-content-between align-items-center"
+          style={{ marginTop: "-4%" }}
         >
 
-         
-
-          {/* <a class="white-text mx-3">Allow Access</a> */}
-
-        
 
         </div>
         <div class="container mt-3">
@@ -50,11 +82,11 @@ export default function Goods() {
                     <td>{v.internalReference}</td>
                     <td>{v.asset}</td>
                     <td>{v.quantity.value}</td>
-                    <td></td>
+                    <td>Yes</td>
 
                     <td>
                       <span type="button" class="btn btn-warning btn-rounded" data-toggle="modal" data-target="#myModal"
-                       onClick={() => setItem(v)}
+                       onClick={() => handleShow(setItem(v))}
                       >View</span>
                     </td>
 
@@ -69,65 +101,55 @@ export default function Goods() {
       </div>
 
 
-      <div class="modal" id="myModal">
-        <div class="modal-dialog modal-dialog-scrollable-sm">
-          <div class="modal-content" style={{ width: "115%" }}>
 
-            {/* <!-- Modal Header --> */}
-            <div class="modal-header">
-              <h3 class="modal-title">Murabaha Agreements Detail</h3>
-              <button type="button" class="btn btn-danger close" data-dismiss="modal">X</button>
-            </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Goods Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        {(item != null) ?
+            // return 
+            (
+              <div class="modal-body">
+                <table id="customers">
 
-            {/* <!-- Modal body --> */}
-            
-            {GoodState.map((v, i) => {
-              return (
-                <div class="modal-body">
-                  <table id="customers">
-
-                    <tr>
-                      <th>Company</th>
-                      <th>Contact</th>
-                    </tr>
-                    <tr>
-                      <td>Seller</td>
-                      <td>{v.vendor.name}</td>
-                    </tr>
-                    <tr>
-                      <td>Asset</td>
-                      <td>{v.description}</td>
-                    </tr>
-                    <tr>
-                      <td>Quantity</td>
-                      <td>{v.quantity.value}</td>
-                    </tr>
-                    <tr>
-                      <td>Reedemable</td>
-                      <td>Yes</td>
-                    </tr>
-                    <tr>
-                      <td>Issure</td>
-                      <td>No</td>
-                    </tr>
+                  <tr>
+                    <th>Company</th>
+                    <th>Contact</th>
+                  </tr>
+                  <tr>
+                    <td>Vendor</td>
+                    <td>{item.vendor.name}</td>
+                  </tr>
+                  <tr>
+                    <td>Asset</td>
+                    <td>{item.description}</td>
+                  </tr>
+                  <tr>
+                    <td>Quantity</td>
+                    <td>{item.quantity.value}</td>
+                  </tr>
+                  <tr>
+                    <td>Reedemable</td>
+                    <td>Yes</td>
+                  </tr>
+                  <tr>
+                    <td>Insured</td>
+                    <td>Yes</td>
+                  </tr>
 
 
-                  </table>
+                </table>
 
-                </div>
-              )
-            })}
+              </div>
+            ):<></>
+          }
 
-
-
-            {/* <!-- Modal footer --> */}
-            <div class="modal-footer">
-              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-            </div>
-
-          </div>
+        </Modal.Body>
+        <div class="modal-footer d-flex justify-content-evenly">
+          {loading ? <button type="button" class="btn btn-success" data-dismiss={show} onClick={Redeem} >Redeem</button> : <Spin size="large" />}
         </div>
-      </div>
+      </Modal>
 
 
     </div>
