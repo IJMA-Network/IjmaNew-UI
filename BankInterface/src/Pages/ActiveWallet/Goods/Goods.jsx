4627734c8d032aae8e15cd031cc1006aa7092030
Syed Muhemin Ali <149074986+muhemin123@users.noreply.github.com>
2023-10-27 05:48:25 -0700

    Goods Pagination added
    Goods Pagination added

--- BankInterface/src/Pages/ActiveWallet/Goods/Goods.jsx
+++ BankInterface/src/Pages/ActiveWallet/Goods/Goods.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext,useEffect } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import Filter from '../../filter/filter';
 import './Goods.css';
 import GoodState from './GoodsState.json';
@@ -8,6 +8,11 @@ import { ToastContainer, toast } from 'react-toastify';
 import StoreContext from '../../../ContextApi';
 import { Button, message, Space, Spin } from 'antd';
 import Modal from 'react-bootstrap/Modal';
+// pagination import here
+import GoodsPagination from "../../Pagination";
+
+
+let itemsPerPage = 1;
 
 export default function Goods() {
   const [user, setUser] = useState({ accountName: "seller1" });
@@ -18,6 +23,10 @@ export default function Goods() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  //  new state pagination here
+  const [page, setPage] = useState(1);
+  const totalPages = Math.ceil(goods?.length / itemsPerPage)
 
   // console.log(contextData.SignInData, "Good Context Data");
 
@@ -38,12 +47,12 @@ export default function Goods() {
   useEffect(() => {
     setUser(contextData.SignInData);
     let payload = {
-        account: user.UserAccountNo,
-        consumable: ""
+      account: user.UserAccountNo,
+      consumable: ""
     }
     getData("owned-goods", payload, setGoods);
     // console.log("goods in seller",goods);
-}, [user])
+  }, [user])
 
   const Redeem = () => {
     setloading(false)
@@ -65,10 +74,21 @@ export default function Goods() {
   //   theme: "light",
   // })
 
+  // pagination function here
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+
+  const displayedData = goods.slice(
+    (page - 1) * itemsPerPage,
+    page * itemsPerPage
+  );
+
+
   return (
     <div>
       <div class="card card-cascade narrower">
-        <Filter data={{ GoodState, setGoods }}/>
+        <Filter data={{ GoodState, setGoods }} />
         <div
           class="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 d-flex justify-content-between align-items-center"
           style={{ marginTop: "-4%" }}
@@ -89,28 +109,28 @@ export default function Goods() {
                 <th></th>
               </tr>
             </thead>
-            {goods.map((v, i) => {
+            {displayedData?.map((v, i) => {
               return (
                 <tbody>
                   <tr>
-                    <td>{v.internalReference}</td>
-                    <td>{v.asset}</td>
-                    <td>{v.quantity.value}</td>
+                    <td>{v?.internalReference}</td>
+                    <td>{v?.asset}</td>
+                    <td>{v?.quantity?.value}</td>
                     <td>Yes</td>
 
                     <td>
                       <span type="button" class="btn btn-warning btn-rounded" data-toggle="modal" data-target="#myModal"
-                       onClick={() => handleShow(setItem(v))}
+                        onClick={() => handleShow(setItem(v))}
                       >View</span>
                     </td>
-
                   </tr>
                 </tbody>
               )
             })}
-
-
           </table>
+        </div>
+        <div style={{display:"flex",justifyContent:'center',alignItems:"center"}}>
+          <GoodsPagination count={totalPages} page={page} onChange={handlePageChange} data={goods}/>
         </div>
       </div>
 
@@ -121,7 +141,7 @@ export default function Goods() {
           <Modal.Title>Goods Details</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        {(item != null) ?
+          {(item != null) ?
             // return 
             (
               <div class="modal-body">
@@ -156,7 +176,7 @@ export default function Goods() {
                 </table>
 
               </div>
-            ):<></>
+            ) : <></>
           }
 
         </Modal.Body>
