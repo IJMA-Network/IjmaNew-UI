263a5fd3e18355a88580a73901e3e317634ac23a
Syed Muhemin Ali <149074986+muhemin123@users.noreply.github.com>
2023-10-27 05:50:31 -0700

    Goods Css Bug Solve
    Goods Css Bug Solve

--- BankInterface/src/Pages/ActiveWallet/Goods/Goods.jsx
+++ BankInterface/src/Pages/ActiveWallet/Goods/Goods.jsx
@@ -87,20 +87,20 @@ export default function Goods() {
 
   return (
     <div>
-      <div class="card card-cascade narrower">
+      <div className="card card-cascade narrower">
         <Filter data={{ GoodState, setGoods }} />
         <div
-          class="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 d-flex justify-content-between align-items-center"
+          className="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 d-flex justify-content-between align-items-center"
           style={{ marginTop: "-4%" }}
         >
 
 
         </div>
-        <div class="container mt-3">
+        <div className="container mt-3">
           <h2 className='text-center'>Goods</h2>
 
-          <table class="table table-hover">
-            <thead class="bg-light">
+          <table className="table table-hover">
+            <thead className="bg-light">
               <tr>
                 <th>Consignmento.</th>
                 <th>Asset</th>
@@ -119,7 +119,7 @@ export default function Goods() {
                     <td>Yes</td>
 
                     <td>
-                      <span type="button" class="btn btn-warning btn-rounded" data-toggle="modal" data-target="#myModal"
+                      <span type="button" className="btn btn-warning btn-rounded" data-toggle="modal" data-target="#myModal"
                         onClick={() => handleShow(setItem(v))}
                       >View</span>
                     </td>
@@ -144,7 +144,7 @@ export default function Goods() {
           {(item != null) ?
             // return 
             (
-              <div class="modal-body">
+              <div className="modal-body">
                 <table id="customers">
 
                   <tr>
@@ -153,15 +153,15 @@ export default function Goods() {
                   </tr>
                   <tr>
                     <td>Vendor</td>
-                    <td>{item.vendor.name}</td>
+                    <td>{item?.vendor?.name}</td>
                   </tr>
                   <tr>
                     <td>Asset</td>
-                    <td>{item.description}</td>
+                    <td>{item?.description}</td>
                   </tr>
                   <tr>
                     <td>Quantity</td>
-                    <td>{item.quantity.value}</td>
+                    <td>{item?.quantity?.value}</td>
                   </tr>
                   <tr>
                     <td>Reedemable</td>
@@ -180,8 +180,8 @@ export default function Goods() {
           }
 
         </Modal.Body>
-        <div class="modal-footer d-flex justify-content-evenly">
-          {loading ? <button type="button" class="btn btn-success" data-dismiss={show} onClick={Redeem} >Redeem</button> : <Spin size="large" />}
+        <div className="modal-footer d-flex justify-content-evenly">
+          {loading ? <button type="button" className="btn btn-success" data-dismiss={show} onClick={Redeem} >Redeem</button> : <Spin size="large" />}
         </div>
       </Modal>
 
